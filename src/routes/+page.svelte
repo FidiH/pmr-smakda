052733b0479b5413b68379a8859433810e41e3de
root <root@localhost.localdomain>
2025-10-29 10:20:06 +0000

    Setup projek dasar banget

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+
+<div class="p-7">
+<h1 class="font-bold text-2xl">Fidi Hartato</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis quaerat praesentium atque quo ipsum, dolorem quod perspiciatis tempore maiores, cum eum excepturi nemo iusto dignissimos sapiente sunt nisi corporis fugiat.</p>
+</div>
